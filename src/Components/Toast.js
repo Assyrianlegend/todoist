5b1e7d937dd48f6280a854d6b36a3fee931feb6d
Assyrianlegend <belbron2@yahoo.com>
2023-02-02 18:59:40 -0800

    belbron commiting on server

--- src/Components/Toast.js
+++ src/Components/Toast.js
@@ -11,16 +11,13 @@ function clearToast(toastIndex,closeStyle){
 }
   return (
      <div >
-      <div className='toast-container'>
         {toast &&
-          <div className={TContext.classes}>
-              <span>{TContext.toasts[0].text}</span>
-              <a className="undo" href="">Undo</a>
-              <svg onClick={(e) => {e.preventDefault(); clearToast(0,'fade-out')}} className='close-icon cursor-pointer' xmlns="http://www.w3.org/2000/svg" style={{transform: "rotate(45deg)"}}  viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>
-          </div>
-          }
-      </div>
-       
+        <div className={TContext.classes}>
+            <span>{TContext.toasts[0].text}</span>
+            <a className="undo" href="">Undo</a>
+            <svg onClick={(e) => {e.preventDefault(); clearToast(0,'fade-out')}} className='close-icon cursor-pointer' xmlns="http://www.w3.org/2000/svg" style={{transform: "rotate(45deg)"}}  viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>
+        </div>
+    }
     </div>
 
   );
